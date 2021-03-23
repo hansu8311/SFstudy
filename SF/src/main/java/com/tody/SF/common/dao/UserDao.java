112776047d4ed7ea37563333bd443f8fa93f2cc3
KumHanSu <cjsdyd453@naver.com>
2021-03-23 11:04:05 +0900

    템플릿 완료

--- SF/src/main/java/com/tody/SF/common/dao/UserDao.java
+++ SF/src/main/java/com/tody/SF/common/dao/UserDao.java
@@ -5,105 +5,58 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 
 import com.tody.SF.common.dto.User;
 
 public class UserDao {
-	private DataSource dataSource;//초기에 설정하면 사용중에는 바뀌지 않는 읽기전용 인스턴스 변수
-	private JdbcContext jdbcContext;
+	
+	private RowMapper<User> userMapper = new RowMapper<User>() {
+		@Override
+		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+			User user = new User();
+			user.setId(rs.getString("id"));
+			user.setName(rs.getString("name"));
+			user.setPassword(rs.getString("password"));
+			return user;
+		}
+	};
+	
+	private JdbcTemplate jdbcTemplate;
 	
 	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
-	}
-	
-	public void setJdbcContext(JdbcContext jdbcContext) {
-		this.jdbcContext = jdbcContext;
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 	
 	public void add(final User user) throws SQLException{
-		 jdbcContext.workWithStatementStrategy(
-				 new StatementStrategy() {//익명내부클래스
-					@Override
-					public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-						PreparedStatement ps = c.prepareStatement("insert into users (id, name, password) values(?,?,?)");
-						ps.setString(1, user.getId());
-						ps.setString(2, user.getName());
-						ps.setString(3, user.getPassword());
-						
-						return ps;
-					}
-				});
+		this.jdbcTemplate.update("insert into users (id, name, password) values(?,?,?)", user.getId(), user.getName(), user.getPassword());
 	}
 	
 	public void deleteAll() throws SQLException{
-		this.jdbcContext.executeSql("DELETE FROM Users");
+		this.jdbcTemplate.update("delete from users");
 	}
 	
 	public User get(String id) throws ClassNotFoundException, SQLException{
-		
-		 Connection c = dataSource.getConnection();
-		 
-		 PreparedStatement ps = c.prepareStatement(
-				 "SELECT * FROM users where id = ?");
-		 ps.setString(1, id);
-	
-		 ResultSet rs = ps.executeQuery();
-		 
-		 User user = null;
-		 if(rs.next()) {
-			 user = new User();
-			 user.setId(rs.getString("id"));
-			 user.setName(rs.getString("name"));
-			 user.setPassword(rs.getString("password"));
-		 }
-		 
-		 rs.close();
-		 ps.close();
-		  
-		 c.close();
-		 
-		 if(user==null) throw new EmptyResultDataAccessException(1);
-		 
-		 return user;
+		return this.jdbcTemplate.queryForObject("SELECT * FROM users where id = ?", 
+				new Object[] {id},
+				this.userMapper);
 	}
 
-	public int getCount() throws ClassNotFoundException, SQLException{
-		
-		Connection c = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		try {
-			c = dataSource.getConnection();
-			ps = c.prepareStatement("SELECT count(*) AS TCOUNT FROM users");
-			
-			rs = ps.executeQuery();
-			rs.next();
-			return rs.getInt(1);
-		} catch (SQLException e) {
-			throw e;
-		}finally {
-			if(rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
-				}
-			}
-			if(ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-				}
-			}
-			if(c != null) {
-				try {
-					c.close();
-				} catch (SQLException e) {
-				}
-			}
-		}
+	public int getCount() throws ClassNotFoundException, SQLException{		
+		return this.jdbcTemplate.queryForInt("SELECT count(*) AS TCOUNT FROM users");
+	}
+	
+	public List<User> getAll() throws ClassNotFoundException, SQLException{
+		return this.jdbcTemplate.query("SELECT * FROM users order by id", 
+		this.userMapper);	
 	}
 }
