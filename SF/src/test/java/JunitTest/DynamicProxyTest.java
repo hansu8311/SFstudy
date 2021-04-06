5d9ca6d5b9c1c091d892b33914234164bac16318
KumHanSu <cjsdyd453@naver.com>
2021-04-06 20:47:25 +0900

    스프링이 제공해주는 ProxyFactoryBean을 이용하여 다이나믹 프록시 메소드 구현 및 테스트 (MethodInterceptor :타겟오브젝트를 전달할 필요가없다. )

---
+++ SF/src/test/java/JunitTest/DynamicProxyTest.java
@@ -0,0 +1,70 @@
+package JunitTest;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.lang.reflect.Proxy;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
+
+import com.tody.SF.common.dao.Interface.Hello;
+import com.tody.SF.common.service.HelloTarget;
+import com.tody.SF.common.service.HelloUppercase;
+import com.tody.SF.common.service.UppercaseHandler;
+
+public class DynamicProxyTest {
+
+
+	@Test
+	public void simpleProxy() {
+		Hello proxiedHello = (Hello)Proxy.newProxyInstance(
+				getClass().getClassLoader()
+				, new Class[] {Hello.class}
+				, new UppercaseHandler(new HelloTarget()));
+		//assertThat(hello.sayHello("Toby"), is("Hello Toby"));
+		//assertThat(hello.sayHi("Toby"), is("Hi Toby"));
+		//assertThat(hello.sayThankYou("Toby"), is("Thank You Toby"));
+		
+		Hello proxiHello = new HelloUppercase(new HelloTarget());
+		assertThat(proxiHello.sayHello("Toby"), is("HELLO TOBY"));
+		assertThat(proxiHello.sayHi("Toby"), is("HI TOBY"));
+		assertThat(proxiHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
+	}
+	
+	@Test
+	public void ProxyTest() {
+		Hello proxiedHello = (Hello)Proxy.newProxyInstance(
+				getClass().getClassLoader()
+			   ,new Class[] {Hello.class}
+			   ,new UppercaseHandler(new HelloTarget()));
+		
+		assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+		assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+		assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
+	}
+	
+	@Test
+	public void ProxyFactoryBeanTest() {
+		ProxyFactoryBean pfBean = new ProxyFactoryBean();
+		pfBean.setTarget(new HelloTarget());
+		pfBean.addAdvice(new UppercaseAdvice());
+		
+		Hello proxiedHello = (Hello)pfBean.getObject();
+		assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+		assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+		assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
+	}
+	
+	static class UppercaseAdvice implements MethodInterceptor{
+
+		@Override
+		public Object invoke(MethodInvocation invocation) throws Throwable {
+			String ret = (String)invocation.proceed();//타깃오브젝트를 전달할 필요가 없다.
+			return ret.toUpperCase();
+		}
+		
+	}
+}
