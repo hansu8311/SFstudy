e5d1e6e0d752430dc38aff8c5155e563707cfaf4
KumHanSu <cjsdyd453@naver.com>
2021-04-12 16:19:16 +0900

    AspectJ 포인트컷 표현식

--- SF/src/test/java/JunitTest/PointCutTest.java
+++ SF/src/test/java/JunitTest/PointCutTest.java
@@ -25,6 +25,29 @@ public class PointCutTest {
 		
 		assertThat(pointcut.getClassFilter().matches(Target.class) && pointcut.getMethodMatcher().matches(Target.class.getMethod("plus", int.class, int.class), null), is(false));
 		
-		assertThat(pointcut.getClassFilter().matches(Bean.class) && pointcut.getMethodMatcher().matches(Target.class.getMethod("method", int.class, int.class), null), is(false));
+		assertThat(pointcut.getClassFilter().matches(Bean.class) && pointcut.getMethodMatcher().matches(Bean.class.getMethod("method", int.class, int.class), null), is(false));
+	}
+	@Test
+	public void pointcut() throws Exception{
+		targetClassPointcutMatches("execution(* *(..))", true, true, true, true, true, true);
+	}
+	
+	
+	public void targetClassPointcutMatches(String expression, boolean... expected) throws Exception{
+		pointcutMatches(expression, expected[0], Target.class, "hello");
+		pointcutMatches(expression, expected[1], Target.class, "hello", String.class);
+		pointcutMatches(expression, expected[2], Target.class, "plus", int.class, int.class);
+		pointcutMatches(expression, expected[3], Target.class, "minus", int.class, int.class);
+		pointcutMatches(expression, expected[4], Target.class, "method");
+		pointcutMatches(expression, expected[5], Bean.class, "method");
+		
+		}
+	public void pointcutMatches(String expression, Boolean expected, Class<?> clazz, String methodname, Class<?>... args) throws Exception{
+		AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+		pointcut.setExpression(expression);
+		
+		assertThat(pointcut.getClassFilter().matches(clazz) 
+				&& pointcut.getMethodMatcher().matches(clazz.getMethod(methodname,args), null), is(expected));
 	}
 }
+
